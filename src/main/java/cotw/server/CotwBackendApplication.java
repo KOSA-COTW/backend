27305cfc2d4f97d971e4137315ba5cb0e5c1fdba
jaeyong <ljy3573057@gmail.com>
2025-07-22 10:12:06 +0900

    Initial commit

---
+++ src/main/java/cotw/server/CotwBackendApplication.java
@@ -0,0 +1,13 @@
+package cotw.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotwBackendApplication.class, args);
+    }
+
+}
