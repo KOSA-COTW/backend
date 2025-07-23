87611034622e806cfb1f219e2eb5584cc76b2aa6
박주성 <121250776+Parkjuseong319@users.noreply.github.com>
2025-07-23 15:46:23 +0900

    feat: 역할 추가

--- src/main/java/cotw/server/domain/member/entity/Role.java
+++ src/main/java/cotw/server/domain/member/entity/Role.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    ADMIN("관리자"), USER("일반유저");
+    ADMIN("관리자"), USER("일반유저"), ORGANIZATION("단체");
 
     private final String description;
 
