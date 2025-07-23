1388fb8a94d48e903bbaaa94624ba0923b414b6c
erinrin <jdw5144@naver.com>
2025-07-23 17:11:23 +0900

    feat : comment entity 추가

--- src/main/java/cotw/server/domain/board/domain/Comment.java
+++ src/main/java/cotw/server/domain/board/domain/Comment.java
@@ -1,11 +1,16 @@
 package cotw.server.domain.board.domain;
 
 import cotw.server.common.BaseEntity;
+import cotw.server.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
@@ -27,4 +32,41 @@ public class Comment extends BaseEntity {
     private Post post;
 
     private String content;
+    // ---- 아래 추가 ----
+    // 대댓글 기능
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL)
+    private List<Comment> childComments = new ArrayList<>();
+
+    // 소프트 삭제
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    private LocalDateTime deletedAt;
+
+    // 좋아요 개수
+    @Column(nullable = false)
+    private int likeCount = 0;
+
+    // 연관관계 편의 메서드
+    public void addChildComment(Comment child) {
+        this.childComments.add(child);
+        child.parentComment = this;
+    }
+
+    public void softDelete() {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void increaseLikeCount() {
+        this.likeCount++;
+    }
+
+    public void decreaseLikeCount() {
+        if (this.likeCount > 0) this.likeCount--;
+    }
 }
